c463c377f1f9c2b4718b4793f9f5f2e95310d506
Melwin T Joshy <95244119+mj465@users.noreply.github.com>
2024-10-01 16:01:34 +0530

    Add files via upload

---
+++ BST.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct bstNode
+{
+	int val;
+	struct bstNode *left, *right;
+};
+
+//create new node fn
+struct bstNode* newNodeCreate(int data)
+{
+	struct bstNode *temp = (struct bstNode*) malloc(sizeof(bstNode));
+	temp->val = data;
+	temp->left = temp->right = NULL;
+	return temp;
+}
+
+//insert fn
+struct bstNode* insertNode(struct bstNode* node, int data)
+{
+	if (node == NULL)
+	{
+		return newNodeCreate(data);
+	}
+	if (data < node->val)
+	{
+		node->left = insertNode(node->left, data);
+	}
+	else if (data > node->val)
+	{
+		node->right = insertNode(node->right, data);
+	}
+	
+	return node;
+}
+
+//find min fn
+
+struct bstNode* findMin(struct bstNode* root)
+{
+	if (root == NULL)
+	{
+		return NULL; 
+	}
+	else if (root->left != NULL)
+	{
+		return findMin(root->left);
+	}
+	return root;
+}
+
+//delete fn
+struct bstNode* deleteNode(struct bstNode* root, data)
+{
+	if (root == NULL)
+	{
+		return NULL;
+	}
+	if (data > root->val)
+	{
+		root->right = deleteNode(root->right, data); 
+	}
+	else if (data < root->val)
+	{
+		root->left = deleteNode(root->left, data);
+	}
+	else 
+	{
+		if (root->left == NULL && root->right == NULL)
+			{
+				 free(root);
+				 return NULL;
+			}
+		
+			
+			
+	}
+	
+}
